9cbb6b2957a4f1a53cd7f49f75a0b04a55c640bc
Salanoid <salanoid@gmail.com>
2017-02-28 14:26:21 +0200

    some beautify

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -62,13 +62,13 @@ class CuisinesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_cuisine
-      @cuisine = Cuisine.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_cuisine
+    @cuisine = Cuisine.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def cuisine_params
-      params.require(:cuisine).permit(:title, :about, :availability, :location, :average_price)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def cuisine_params
+    params.require(:cuisine).permit(:title, :about, :availability, :location, :average_price)
+  end
 end
