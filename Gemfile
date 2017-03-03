a359c7549d495cddbd0d3de172bb69cc8e0b86bb
Salanoid <salanoid@gmail.com>
2017-03-03 19:27:43 +0200

    foo

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ end
 gem 'bootstrap', '~> 4.0.0.alpha6'
 gem 'devise'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.0.1'
+gem 'rails', '~> 5.0.2'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.18'
 # Use Puma as the app server
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :cuisines do
     resources :recipes
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
