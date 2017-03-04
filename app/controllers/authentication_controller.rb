c4b1018f922ca506bb3e4241bb8030e0f453dc15
Salanoid <salanoid@gmail.com>
2017-03-04 10:55:22 +0200

    add user

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,4 @@
+class AuthenticationController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :chefs
   resources :cuisines do
     resources :recipes
   end
+
+  resources :authentication
 
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
