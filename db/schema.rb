02b57efac89791e6f4df50d6ff7996cfc340a64b
Salanoid <salanoid@gmail.com>
2017-03-08 11:51:36 +0200

    remove navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304083542) do
+ActiveRecord::Schema.define(version: 20170307125858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,19 @@ ActiveRecord::Schema.define(version: 20170304083542) do
     t.index ["cuisine_id"], name: "index_recipes_on_cuisine_id", using: :btree
   end
 
+  create_table "reservations", force: :cascade do |t|
+    t.string   "name"
+    t.time     "when"
+    t.integer  "no_of_persons"
+    t.text     "message"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "user_id"
+    t.integer  "cuisine_id"
+    t.index ["cuisine_id"], name: "index_reservations_on_cuisine_id", using: :btree
+    t.index ["user_id"], name: "index_reservations_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -74,4 +87,6 @@ ActiveRecord::Schema.define(version: 20170304083542) do
   add_foreign_key "cuisines", "chefs"
   add_foreign_key "recipes", "chefs"
   add_foreign_key "recipes", "cuisines"
+  add_foreign_key "reservations", "cuisines"
+  add_foreign_key "reservations", "users"
 end
