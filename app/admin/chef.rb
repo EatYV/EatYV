9aac3a414a71db1841f50d68f3d5bbb86cb38085
Salanoid <salanoid@gmail.com>
2017-03-15 20:10:52 +0200

    activeadmin dashboard

---
+++ app/admin/chef.rb
@@ -0,0 +1,16 @@
+ActiveAdmin.register Chef do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+  permit_params :email, :sign_in_count, :current_sign_in_at, :current_sign_in_ip
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
