5f469b325f4c23ba812406ad1ba97ee32aed9cda
Salanoid <salanoid@gmail.com>
2017-03-17 00:12:51 +0200

    foo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,26 @@
 Rails.application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # Code is not reloaded between requests.
   config.cache_classes = true
+
+  # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both threaded web servers
+  # and those relying on copy on write to perform better.
+  # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-  config.consider_all_requests_local = false
+
+  # Full error reports are disabled and caching is turned on.
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+
+  # Compress JavaScripts and CSS.
+  config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+
   config.serve_static_assets = false
   config.action_mailer.raise_delivery_errors = false
   config.active_support.deprecation = :log
