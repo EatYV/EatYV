c335f45a22b94d6d23ad0955b66653cdd3197c02
Salanoid <salanoid@gmail.com>
2017-03-16 23:55:00 +0200

    foo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,14 +1,16 @@
 Rails.application.configure do
   config.cache_classes = true
-  config.eager_load = true
-  config.consider_all_requests_local = false
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.assets.compile = true
-  config.action_mailer.raise_delivery_errors = false
-  config.active_support.deprecation = :log
-  config.active_record.migration_error = :page_load
-  config.assets.debug = true
+  config.serve_static_assets = false
+
+  config.assets.compress = true
+  config.assets.compile = false
   config.assets.digest = true
-  config.assets.raise_runtime_errors = true
-  config.action_view.raise_on_missing_translations = true
+  config.assets.initialize_on_precompile = false
+  config.assets.precompile += %w(login.css)
+
+  config.force_ssl = true
+  config.i18n.fallbacks = true
+  config.active_support.deprecation = :notify
 end
