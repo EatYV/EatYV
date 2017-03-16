f2f9daf2bc3a88cdd63840aae146cdadebc8752f
Salanoid <salanoid@gmail.com>
2017-03-17 00:21:48 +0200

    foo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-
+  config.i18n.fallbacks = true
   config.serve_static_assets = false
   config.action_mailer.raise_delivery_errors = false
   config.active_support.deprecation = :log
