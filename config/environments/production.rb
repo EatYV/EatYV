1708ca9098c4aded47ab977c852c7cdc94879ca9
Salanoid <salanoid@gmail.com>
2017-03-17 01:10:45 +0200

    foo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,16 +14,13 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.compress = true
   config.i18n.fallbacks = true
   config.serve_static_assets = true
   config.action_mailer.raise_delivery_errors = false
