2880ebf6206cdbd3b63390951c0cdc1c6dbfa8e3
Salanoid <salanoid@gmail.com>
2017-03-18 13:37:54 +0200

    phone numbers are ready for users too

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,13 @@ class User < ApplicationRecord
   def login
     @login || self.phone || self.email
   end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(phone) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:phone) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 end
