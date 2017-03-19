b348fd4f173eb0d8b946bb8de70e0a58fd5e0c60
Salanoid <salanoid@gmail.com>
2017-03-19 02:13:20 +0200

    cuisine complete class

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -3,13 +3,12 @@ class Cuisine < ApplicationRecord
   has_many :recipes, dependent: :destroy
   has_many :reservations, dependent: :destroy
   has_many :reviews, dependent: :destroy
-end
 
-def self.search(search)
-  if search
-    find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
-  else
-    find(:all)
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+    else
+      find(:all)
+    end
   end
 end
-
